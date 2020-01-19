19b859f3171b31c876a1725224bca736b72ae6b1
adiM3344 <adi.me123@gamil.com>
2020-01-19 16:40:15 +0200

    commit

--- BestFS.h
+++ BestFS.h
@@ -7,6 +7,7 @@
 #include "State.h"
 #include <deque>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -25,25 +26,28 @@ public:
         vector<State<T>*> closed;
         openList.push_back(s->getInitialState());
         while (openList.size() > 0) {
-            State<T> *top = *(min_element(openList.begin(), openList.end(), Compare()));
+            auto t = (min_element(openList.begin(), openList.end(), Compare()));
+//            State<T> *top = *(min_element(openList.begin(), openList.end(), Compare()));
+            State<T> *top = *t;
             this->nodesEvaluated = 1;
-            openList.erase(top);
+            openList.erase(t);
             closed.push_back(top);
             //
-            if (isGoalState(top->getState())) {
+            if (s->isGoalState(top)) {
                 //path
-                return updateBackTrace(top);
+//                return updateBackTrace(top);
+                return closed;
             }
             vector<State<T>*> successors = s->getAllStates(top);
             if(successors.size()==0){
                 continue;
             }
             for (int i = 0; i < successors.size(); i++) {
-                State<T>* itOpen=find(openList.begin(),openList.end(),successors[i]);
-                State<T>* itClosed=find(closed.begin(),closed.end(),successors[i]);
+                auto itOpen = find(openList.begin(),openList.end(),successors[i]);
+                auto itClosed=find(closed.begin(),closed.end(),successors[i]);
                 if(itOpen !=openList.end() && itClosed!=closed.end()){
                     openList.push_back(successors[i]);
-                    successors[i].setCameFrom(top);
+                    successors[i]->setCameFrom(top);
                 }
                 //if this new path is better than previous one
                 else {
@@ -51,9 +55,9 @@ public:
                         if(itOpen !=openList.end()){
                             openList.push_back(successors[i]);
                         } else{
-                            successors[i].setCameFrom(top);
+                            successors[i]->setCameFrom(top);
                             //update cost
-                            successors[i].setCost(successors[i].getCost()+top->getCost());
+                            successors[i]->setCost(successors[i]->getCost()+top->getCost());
                         }
                     }
                 }
